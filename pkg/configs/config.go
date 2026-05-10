2726bc43eb6618792ab5b0ba498cceeb4d5b4e3a
zy84338719 <zy84338719@hotmail.com>
2026-05-11 02:09:46 +0800

    feat: add MySQL env var overrides for DB_TYPE/DB_HOST/DB_PORT/DB_USER/DB_PASSWORD/DB_NAME

--- pkg/configs/config.go
+++ pkg/configs/config.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -30,13 +31,36 @@ func Init() {
 	WebhookRateLimit = GlobalConfig.Webhook.RateLimit
 	WebhookIPWhitelist = GlobalConfig.Webhook.IPWhitelist
 
-	// Manual override for old ENV vars
 	if secret := os.Getenv("WEBHOOK_SECRET"); secret != "" {
 		WebhookSecret = secret
 		GlobalConfig.Webhook.Secret = secret
 	}
 
+	if dbType := os.Getenv("DB_TYPE"); dbType != "" {
+		GlobalConfig.Database.Type = dbType
+	}
 	if dbPath := os.Getenv("DB_PATH"); dbPath != "" {
 		GlobalConfig.Database.Path = dbPath
+	}
+	if dbHost := os.Getenv("DB_HOST"); dbHost != "" {
+		GlobalConfig.Database.Host = dbHost
+	}
+	if dbPort := os.Getenv("DB_PORT"); dbPort != "" {
+		port := 0
+		fmt.Sscanf(dbPort, "%d", &port)
+		if port > 0 {
+			GlobalConfig.Database.Port = port
+		}
+	}
+	if dbUser := os.Getenv("DB_USER"); dbUser != "" {
+		GlobalConfig.Database.User = dbUser
+	}
+	if dbPassword := os.Getenv("CONFIG_DATABASE_PASSWORD"); dbPassword != "" {
+		GlobalConfig.Database.Password = dbPassword
+	} else if dbPassword := os.Getenv("DB_PASSWORD"); dbPassword != "" {
+		GlobalConfig.Database.Password = dbPassword
+	}
+	if dbName := os.Getenv("DB_NAME"); dbName != "" {
+		GlobalConfig.Database.DBName = dbName
 	}
 }
